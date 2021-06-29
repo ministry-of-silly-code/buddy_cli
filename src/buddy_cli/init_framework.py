e3531521c7d894546ae3679ef508f2c6d5b4ce33
DrTtnk <pietrobelgioco@gmail.com>
2021-06-29 15:49:44 +0200

    Spinners and prompts

--- src/buddy_cli/init_framework.py
+++ src/buddy_cli/init_framework.py
@@ -1,8 +1,20 @@
 import argparse
-from typing import Optional, Callable
+import inspect
+import os
+from typing import Optional, Callable, Union, Dict, Any
+
+from halo import Halo
 
 
-class WorkingDirectory():
+class FatalException(Exception):
+    pass
+
+
+def exec_in_venv(python_command):
+    os.system(f'venv/bin/python {python_command}')
+
+
+class WorkingDirectory:
     def __init__(self, *, path, force):
         if force:
             os.system(f"rm -rf {path}")
@@ -21,18 +33,42 @@ class WorkingDirectory():
         os.chdir(self.current_path)
 
 
-def prompt(name, action: Callable, force_to: Optional[bool] = None):
+def prompt_bool(action: Callable[[], None], parsed_args: Dict[str, Any]):
+    force_to = parsed_args[action.__name__]
+
     if force_to is None:
-        apply = input(f"Do you want to {name.replace('_', ' ')}? [Yn] ") in ['', 'y', 'Y']
+        apply = input(f"Do you want to {action.__name__.replace('_', ' ')}? [Yn] ") in ['', 'y', 'Y']
     else:
         apply = force_to
 
     if apply:
-        action()
+        with Halo(text=action.__name__.replace('_', ' '), spinner='dots'):
+            action()
+
+    return apply
 
 
-def add_function_toggle(self, fn):
-    self.add_argument(f'--{fn.__name__}', help=fn.__doc__, action='store_true', default=False)
+def prompt_string(action: Callable[[str], None], prompt: str, parsed_args: Dict[str, Any]):
+    force_to = parsed_args[action.__name__]
+
+    if force_to is None:
+        param = input(f"Do you want to {action.__name__.replace('_', ' ')}? {prompt} [Leave empty to skip]")
+    else:
+        param = force_to
+
+    if param:
+        with Halo(text=action.__name__.replace('_', ' '), spinner='dots'):
+            action(param)
+
+    return param
 
 
-argparse.ArgumentParser.add_function_toggle = add_function_toggle
+def add_action_toggle(self, fn: Union[Callable[[], None], Callable[[str], None]]):
+    params = list(inspect.signature(fn).parameters.keys())
+    if params:
+        self.add_argument(f'--{fn.__name__}', help=fn.__doc__, default=None)
+    else:
+        self.add_argument(f'--{fn.__name__}', help=fn.__doc__, action='store_true', default=None)
+
+
+argparse.ArgumentParser.add_action_toggle = add_action_toggle
--- src/buddy_cli/init_project.py
+++ src/buddy_cli/init_project.py
@@ -1,53 +1,117 @@
+import shutil
+import subprocess
 import traceback
+from os.path import expanduser
+
+import fabric
+import paramiko
 from git import Repo
 import os
 import venv
 import argparse
+from distutils.dir_util import copy_tree
 
-from . import init_framework
-
-
-class Init:
-    @staticmethod
-    def create_venv():
-        '''Create the experiment venv and initializes all the base dependiencies'''
-        venv.EnvBuilder().create('venv')
-
-    @staticmethod
-    def create_git_repo():
-        '''Setups the git repo with the proper git ignores'''
-        GIT_IGNORE = """venv"""
-
-        Repo.init('./.git', bare=True)
-        with open(".gitignore", "w") as gitignore:
-            gitignore.write(GIT_IGNORE)
-
-    @staticmethod
-    def create_base_structure():
-        '''initializes the base example'''
-        os.mkdir("src")
+from .init_framework import FatalException, WorkingDirectory, prompt_bool, prompt_string
 
 
-opt_in_functions = {k: v for k, v in Init.__dict__.items() if "staticmethod" in str(v)}
+def create_venv():
+    """Create the experiment venv and initializes all the base dependencies"""
+    venv.EnvBuilder(with_pip=True).create('venv')
+    subprocess.check_output(f'venv/bin/python3 -m pip install --upgrade pip', shell=True)
+    print(f"""
+    
+    Remember to source your new environment with:
+        source {os.getcwd()}/venv/bin/activate
+    """)
+
+
+def create_git_repo():
+    """Setups the git repo with the proper git ignores"""
+    GIT_IGNORE = """venv"""
+
+    Repo.init('./.git', bare=True)
+    with open(".gitignore", "w") as gitignore:
+        gitignore.write(GIT_IGNORE)
+
+
+def create_base_structure():
+    """initializes the base example"""
+    base_skeleton_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'base_skeleton')
+    copy_tree(base_skeleton_path, '.')
+    subprocess.check_output(f'venv/bin/python3 -m pip install -r requirements.txt', shell=True)
+
+
+def setup_mila_user(mila_user):
+    try:
+        fabric.Connection(host='login.server.mila.quebec', user=mila_user, connect_timeout=10).run("")
+    except paramiko.ssh_exception.SSHException:
+        print(f"""
+Error while checking SSH connection, stopping
+Did you:
+ - double check that your username is '{mila_user}'?
+ - setup the public and private key for you and for the mila cluster?
+        """)
+        raise FatalException()
+
+    mila_config = f"""
+Host mila1
+    Hostname login-1.login.server.mila.quebec
+Host mila2
+    Hostname login-2.login.server.mila.quebec
+Host mila3
+    Hostname login-3.login.server.mila.quebec
+Host mila4
+    Hostname login-4.login.server.mila.quebec
+Host mila
+    Hostname         login.server.mila.quebec
+Host mila*
+    Port 2222
+
+Match host *.mila.quebec,*.umontreal.ca
+    User {mila_user}
+    PreferredAuthentications publickey,keyboard-interactive
+    Port 2222
+    ServerAliveInterval 120
+    ServerAliveCountMax 5
+"""
+    config_path = expanduser('~/.ssh/config')
+    with open(config_path, mode='r') as config_file:
+        current_config = config_file.read()
+
+    if 'Host mila' not in current_config:
+        shutil.copy(config_path, f'{config_path}_BACKUP')
+        with open(config_path, mode='w') as config_file:
+            config_file.write(current_config + mila_config)
+
+
+def setup_wandb():
+    subprocess.check_output(f'venv/bin/python3 -m pip install wandb', shell=True)
+    subprocess.check_output(f'venv/bin/python3 -m wandb init', shell=True)
 
 
 def init(args=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("--destination", help="The destination of your epic project! :D", required=True)
     parser.add_argument('--force', help="Starts the project from scratch", action='store_true', default=False)
-    for action in opt_in_functions.values():
-        parser.add_function_toggle(action)
+
+    parser.add_action_toggle(setup_mila_user)
+    parser.add_action_toggle(create_venv)
+    parser.add_action_toggle(create_git_repo)
+    parser.add_action_toggle(create_base_structure)
+    parser.add_action_toggle(setup_wandb)
 
     parsed = vars(parser.parse_args(args=args))
 
     base_dir = parsed['destination']
 
     with WorkingDirectory(path=base_dir, force=parsed['force']):
-        for name, action in opt_in_functions.items():
-            prompt(name, create_base_structure, force=parsed[name])
+        os.mkdir("src")
 
-
-    print(f'Hello {parsed.destination}')
+        prompt_string(setup_mila_user, prompt='Insert your Mila username', parsed_args=parsed)
+        prompt_bool(create_venv, parsed_args=parsed)
+        prompt_bool(create_git_repo, parsed_args=parsed)
+        prompt_bool(create_base_structure, parsed_args=parsed)
+        prompt_bool(setup_wandb, parsed_args=parsed)
 
 
 def sys_main():
