095484d6576d93762576675d815bc088a84c3ae3
DrTtnk <pietrobelgioco@gmail.com>
2021-07-06 15:46:55 +0200

    Fix wrong comment

--- src/buddy_cli/init_project.py
+++ src/buddy_cli/init_project.py
@@ -8,7 +8,7 @@ from .init_framework import WorkingDirectory, prompt, add_action_toggle
 
 def init(args=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--destination", help="The destination of your epic project! :D", required=True)
+    parser.add_argument("--dest", help="The destination of your epic project! :D", required=True)
     parser.add_argument('--force', help="Starts the project from scratch", action='store_true', default=False)
     parser.add_argument('--yes', '-y', help="Forces all boolean options to true", action='store_true', default=None)
 
@@ -20,17 +20,17 @@ def init(args=None):
 
     parsed = vars(parser.parse_args(args=args))
 
-    base_dir = parsed['destination']
+    base_dir = parsed['dest']
 
     prompt_with_args = partial(prompt, parsed_args=parsed)
 
     with WorkingDirectory(path=base_dir, force=parsed['force']):
         os.mkdir("src")
 
-        # prompt_with_args(setup_mila_user)
-        # prompt_with_args(partial(setup_wandb, project_name=base_dir))
+        prompt_with_args(setup_mila_user)
+        prompt_with_args(partial(setup_wandb, project_name=base_dir))
 
-        # prompt_with_args(create_git_repo, force=parsed['yes'])
+        prompt_with_args(create_git_repo, force=parsed['yes'])
         has_local_venv = prompt_with_args(create_venv, force=parsed['yes'])
         prompt_with_args(partial(create_base_structure, use_local_venv=has_local_venv), force=parsed['yes'])
 
